ea534acea5a92c722e1797c1527e313a6f20d8c3
consorcioCAP1 <omar.dcruz12@gmail.com>
2023-07-26 10:40:57 -0500

    agregando primer commit

---
+++ src/main/java/com/nttdata/bootcamp/debitcardservice/DebitCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.debitcardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebitCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebitCardServiceApplication.class, args);
+	}
+
+}
